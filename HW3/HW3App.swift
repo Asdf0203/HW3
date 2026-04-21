00ab67244ef13b772ad4ba657f43a98b9a2d0a94
Asdf0203 <162975459+Asdf0203@users.noreply.github.com>
2026-04-21 10:02:01 +0800

    Initial Commit

---
+++ HW3/HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3App.swift
+//  HW3
+//
+//  Created by 許哲浚 on 2026/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
