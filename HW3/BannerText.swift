f2fda6043423b4bf4e02ae1d10578cc492fb6cea
Asdf0203 <162975459+Asdf0203@users.noreply.github.com>
2026-05-23 18:34:08 +0800

    hi

---
+++ HW3/BannerText.swift
@@ -0,0 +1,18 @@
+//
+//  BannerText.swift
+//  HW3
+//
+//  Created by 許哲浚 on 2026/5/4.
+//
+
+import SwiftUI
+
+struct BannerText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BannerText()
+}
